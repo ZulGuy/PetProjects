76954d70c98a07f676a1e9373e91d71dfdbbcd5c
ZulGuy <frostborne1@gmail.com>
2025-12-17 12:36:42 +0200

    Added more Specifications for Transaction entity and implemented /api/transactions endpoint and get requests for this endpoint.

--- fintrack/src/main/java/com/studying/fintrack/domain/repositories/TransactionsRepository.java
+++ fintrack/src/main/java/com/studying/fintrack/domain/repositories/TransactionsRepository.java
@@ -1,10 +1,16 @@
 package com.studying.fintrack.domain.repositories;
 
 import com.studying.fintrack.domain.entities.Transaction;
+import java.sql.Timestamp;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransactionsRepository extends JpaRepository<Transaction, Integer> {
 
+  List<Transaction> findByUserId(int userId);
+  List<Transaction> findByBookedAtBetween(Timestamp from, Timestamp to);
+  List<Transaction> findByBookedAt(Timestamp bookedAt);
+  List<Transaction> findByCategoryId(int categoryId);
 }

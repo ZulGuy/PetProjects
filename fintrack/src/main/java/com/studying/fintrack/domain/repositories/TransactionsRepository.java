504e053e2824aaba7ce084349a2b0f80ad6fc823
ZulGuy <frostborne1@gmail.com>
2025-12-24 23:53:21 +0200

    Added new method to TransactionRepository.

--- fintrack/src/main/java/com/studying/fintrack/domain/repositories/TransactionsRepository.java
+++ fintrack/src/main/java/com/studying/fintrack/domain/repositories/TransactionsRepository.java
@@ -1,5 +1,6 @@
 package com.studying.fintrack.domain.repositories;
 
+import com.studying.fintrack.domain.entities.Account;
 import com.studying.fintrack.domain.entities.Transaction;
 import java.sql.Timestamp;
 import java.util.List;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Repository;
 public interface TransactionsRepository extends JpaRepository<Transaction, Integer>,
     JpaSpecificationExecutor<Transaction> {
 
+  Transaction findByIdAndAccount(int id, Account account);
   List<Transaction> findByAccountId(int accountId);
   List<Transaction> findByBookedAtBetween(Timestamp from, Timestamp to);
   List<Transaction> findByBookedAt(Timestamp bookedAt);

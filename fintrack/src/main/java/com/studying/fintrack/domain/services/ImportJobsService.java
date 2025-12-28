cd1fda7774dadcdc21d9252f574607a952af4717
ZulGuy <frostborne1@gmail.com>
2025-12-28 18:18:25 +0200

    Implemented basic ImportJobsService class.

--- fintrack/src/main/java/com/studying/fintrack/domain/services/ImportJobsService.java
+++ fintrack/src/main/java/com/studying/fintrack/domain/services/ImportJobsService.java
@@ -1,6 +1,10 @@
 package com.studying.fintrack.domain.services;
 
+import com.studying.fintrack.domain.entities.ImportJob;
 import com.studying.fintrack.domain.repositories.ImportJobsRepository;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +16,36 @@ public class ImportJobsService {
   @Autowired
   public ImportJobsService(ImportJobsRepository importJobsRepository) {
     this.importJobsRepository = importJobsRepository;
+  }
+
+  public List<ImportJob> getAllImportJobs() {
+    return importJobsRepository.findAll();
+  }
+
+  public ImportJob getImportJobById(int id) {
+    return importJobsRepository.findById(id).orElseThrow(
+        () -> new EntityNotFoundException("Import Job Not Found!")
+    );
+  }
+
+  public ImportJob createImportJob(ImportJob importJob) {
+    return importJobsRepository.save(importJob);
+  }
+
+  public ImportJob updateImportJob(int id, ImportJob importJob) {
+    ImportJob updatedImportJob = importJobsRepository.findById(id).orElse(null);
+    if (updatedImportJob == null)
+      return importJobsRepository.save(importJob);
+    updatedImportJob.setStatus(importJob.getStatus());
+    updatedImportJob.setUser(importJob.getUser());
+    updatedImportJob.setStatsJson(importJob.getStatsJson());
+    updatedImportJob.setCreatedAt(importJob.getCreatedAt());
+    updatedImportJob.setFinishedAt(importJob.getFinishedAt());
+    return importJobsRepository.save(updatedImportJob);
+  }
+
+  public void deleteImportJobById(int id) {
+    importJobsRepository.deleteById(id);
   }
 
 }

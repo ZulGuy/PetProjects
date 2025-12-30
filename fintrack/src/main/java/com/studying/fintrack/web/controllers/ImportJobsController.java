9eebd6e72cff68e7ed570508ca8cc60f203c99fa
ZulGuy <frostborne1@gmail.com>
2025-12-30 23:53:00 +0200

    Implemented some basic methods for ImportJobController.

--- fintrack/src/main/java/com/studying/fintrack/web/controllers/ImportJobsController.java
+++ fintrack/src/main/java/com/studying/fintrack/web/controllers/ImportJobsController.java
@@ -1,7 +1,11 @@
 package com.studying.fintrack.web.controllers;
 
+import com.studying.fintrack.domain.entities.ImportJob;
 import com.studying.fintrack.domain.services.ImportJobsService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +18,16 @@ public class ImportJobsController {
   @Autowired
   public ImportJobsController(ImportJobsService importJobsService) {
     this.importJobsService = importJobsService;
+  }
+
+  @GetMapping
+  public List<ImportJob> getAllImportJobs() {
+    return importJobsService.getAllImportJobs();
+  }
+
+  @PostMapping
+  public ImportJob createImportJob(ImportJob importJob) {
+    return importJobsService.createImportJob(importJob);
   }
 
 }

fba6c0ff426e2202ae52fa5c317368e91b792d3b
ZulGuy <frostborne1@gmail.com>
2025-12-31 16:09:56 +0200

    Implemented some basic methods for ImportJobController.

--- fintrack/src/main/java/com/studying/fintrack/web/controllers/ImportJobsController.java
+++ fintrack/src/main/java/com/studying/fintrack/web/controllers/ImportJobsController.java
@@ -4,7 +4,10 @@ import com.studying.fintrack.domain.entities.ImportJob;
 import com.studying.fintrack.domain.services.ImportJobsService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +31,16 @@ public class ImportJobsController {
   @PostMapping
   public ImportJob createImportJob(ImportJob importJob) {
     return importJobsService.createImportJob(importJob);
+  }
+
+  @PatchMapping("/{id}")
+  public ImportJob updateImportJob(@PathVariable int id, ImportJob importJob) {
+    return importJobsService.updateImportJob(id, importJob);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteImportJob(@PathVariable int id) {
+    importJobsService.deleteImportJobById(id);
   }
 
 }

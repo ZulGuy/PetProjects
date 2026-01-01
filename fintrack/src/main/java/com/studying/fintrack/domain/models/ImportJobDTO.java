1755961cb56b32ff52866b66b800222813642a14
ZulGuy <frostborne1@gmail.com>
2026-01-01 19:53:46 +0200

    Added ImportJobDTO.

---
+++ fintrack/src/main/java/com/studying/fintrack/domain/models/ImportJobDTO.java
@@ -0,0 +1,14 @@
+package com.studying.fintrack.domain.models;
+
+public class ImportJobDTO {
+
+  private String statsJson;
+
+  public String getStatsJson() {
+    return statsJson;
+  }
+
+  public void setStatsJson(String statsJson) {
+    this.statsJson = statsJson;
+  }
+}

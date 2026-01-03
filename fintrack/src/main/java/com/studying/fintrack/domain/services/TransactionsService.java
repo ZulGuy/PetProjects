1c82c5042e08f6cde655115b7aa931456f75a504
ZulGuy <frostborne1@gmail.com>
2026-01-03 17:10:09 +0200

    Refactoring of TransactionsService.

--- fintrack/src/main/java/com/studying/fintrack/domain/services/TransactionsService.java
+++ fintrack/src/main/java/com/studying/fintrack/domain/services/TransactionsService.java
@@ -5,6 +5,7 @@ import com.studying.fintrack.domain.entities.User;
 import com.studying.fintrack.domain.models.TransactionSearchFilter;
 import com.studying.fintrack.domain.repositories.TransactionsRepository;
 import com.studying.fintrack.domain.specifications.TransactionSpecification;
+import com.studying.fintrack.domain.utils.AuthoritiesUtil;
 import jakarta.persistence.EntityNotFoundException;
 import java.sql.Timestamp;
 import java.util.List;
@@ -23,22 +24,25 @@ public class TransactionsService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  /** Це основа, потрібно усі сервісні методи зробити по аналогії цього,
-   * так як тут вже прописано 2 сценарії в залежності від ролі юзера.
-   * Також треба подивитись наскільки сек'юрно написаний цей метод.
+  /**
+   * Це основа, потрібно усі сервісні методи зробити по аналогії цього, так як тут вже прописано 2
+   * сценарії в залежності від ролі юзера. Також треба подивитись наскільки сек'юрно написаний цей
+   * метод.
+   *
    * @return
    */
   public List<Transaction> getAllTransactions() {
-    User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    if(user.getAuthorities().contains("Admin"))
+    User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    if (user.getAuthorities().contains("Admin")) {
       return transactionsRepository.findAll();
+    }
     return transactionsRepository.findByUserId(user.getId());
   }
 
   public Transaction getTransactionById(int id) {
     Transaction transaction;
-    User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    if(user.getAuthorities().contains("Admin")) {
+    User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    if (user.getAuthorities().contains("Admin")) {
       transaction = transactionsRepository.findById(id).orElse(null);
     }
     transaction = transactionsRepository.findByIdAndUserId(id, user.getId()).orElseThrow(
@@ -55,65 +59,56 @@ public class TransactionsService {
   }
 
   public void deleteTransaction(int id) {
-    User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    if(user.getAuthorities().contains("Admin"))
+    User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    if (user.getAuthorities().contains("Admin")) {
       transactionsRepository.deleteById(id);
+    }
     transactionsRepository.deleteByIdAndUserId(id, user.getId());
   }
 
   public Transaction updateTransaction(int id, Transaction transaction) {
-    User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    if(user.getAuthorities().contains("Admin")) {
-      Transaction updatedTransaction = transactionsRepository.findById(id).orElseThrow(
-          () -> new EntityNotFoundException("Transaction not found in DB!")
-      );
-      updatedTransaction.setBookedAt(transaction.getBookedAt());
-      updatedTransaction.setAccount(transaction.getAccount());
-      updatedTransaction.setAmountDecimal(transaction.getAmountDecimal());
-      updatedTransaction.setCategory(transaction.getCategory());
-      updatedTransaction.setCurrency(transaction.getCurrency());
-      updatedTransaction.setRawDescription(transaction.getRawDescription());
-      updatedTransaction.setNote(transaction.getNote());
-      return transactionsRepository.save(updatedTransaction);
-    }
-    Transaction updatedTransaction = transactionsRepository.findByIdAndUserId(id, user.getId()).orElseThrow(
+    User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    Transaction updatedTransaction = AuthoritiesUtil.isAdmin(user)
+        ? transactionsRepository.findById(id).orElseThrow(
         () -> new EntityNotFoundException("Transaction not found in DB!")
-    );
-    updatedTransaction.setBookedAt(transaction.getBookedAt());
-    updatedTransaction.setAccount(transaction.getAccount());
-    updatedTransaction.setAmountDecimal(transaction.getAmountDecimal());
-    updatedTransaction.setCategory(transaction.getCategory());
-    updatedTransaction.setCurrency(transaction.getCurrency());
-    updatedTransaction.setRawDescription(transaction.getRawDescription());
-    updatedTransaction.setNote(transaction.getNote());
+    )
+        : transactionsRepository.findByIdAndUserId(id, user.getId())
+            .orElseThrow(
+                () -> new EntityNotFoundException("Transaction not found in DB!")
+            );
+    toDTO(updatedTransaction, transaction);
     return transactionsRepository.save(updatedTransaction);
   }
 
   public List<Transaction> getTransactionsByAccountId(int accountId) {
-    User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    if(user.getAuthorities().contains("Admin"))
+    User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    if (user.getAuthorities().contains("Admin")) {
       return transactionsRepository.findByAccountId(accountId);
+    }
     return transactionsRepository.findByAccountIdAndUserId(accountId, user.getId());
   }
 
   public List<Transaction> getTransactionsByDate(Timestamp bookedAt) {
-    User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    if(user.getAuthorities().contains("Admin"))
+    User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    if (user.getAuthorities().contains("Admin")) {
       return transactionsRepository.findByBookedAt(bookedAt);
+    }
     return transactionsRepository.findByBookedAtAndUserId(bookedAt, user.getId());
   }
 
   public List<Transaction> getTransactionsByDateBetween(Timestamp from, Timestamp to) {
-    User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    if(user.getAuthorities().contains("Admin"))
+    User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    if (user.getAuthorities().contains("Admin")) {
       return transactionsRepository.findByBookedAtBetween(from, to);
+    }
     return transactionsRepository.findByBookedAtBetweenAndUserId(from, to, user.getId());
   }
 
   public List<Transaction> getTransactionsByCategoryId(int categoryId) {
-    User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    if(user.getAuthorities().contains("Admin"))
+    User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    if (user.getAuthorities().contains("Admin")) {
       return transactionsRepository.findByCategoryId(categoryId);
+    }
     return transactionsRepository.findByCategoryIdAndUserId(categoryId, user.getId());
   }
 
@@ -134,12 +129,25 @@ public class TransactionsService {
     }
     if (filter.from() != null && filter.to() != null) {
       spec = spec.and(
-          TransactionSpecification.betweenDates(Timestamp.valueOf(filter.from()), Timestamp.valueOf(filter.to())));
+          TransactionSpecification.betweenDates(Timestamp.valueOf(filter.from()),
+              Timestamp.valueOf(filter.to())));
     }
     var transactions = transactionsRepository.findAll(spec);
-    if (transactions.stream().toList().isEmpty())
+    if (transactions.stream().toList().isEmpty()) {
       throw new EntityNotFoundException("No transactions found");
+    }
     return transactions.stream().toList();
+  }
+
+  private Transaction toDTO(Transaction updatedTransaction, Transaction transaction) {
+    updatedTransaction.setBookedAt(transaction.getBookedAt());
+    updatedTransaction.setAccount(transaction.getAccount());
+    updatedTransaction.setAmountDecimal(transaction.getAmountDecimal());
+    updatedTransaction.setCategory(transaction.getCategory());
+    updatedTransaction.setCurrency(transaction.getCurrency());
+    updatedTransaction.setRawDescription(transaction.getRawDescription());
+    updatedTransaction.setNote(transaction.getNote());
+    return updatedTransaction;
   }
 
 }

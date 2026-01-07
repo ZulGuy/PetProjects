9bffb46f78ed08e92ff530924c55c16637837ffc
ZulGuy <frostborne1@gmail.com>
2026-01-07 22:30:26 +0200

    Added DTO <--> Entity mapping logic to CategoriesService and ImportJobsService.

--- fintrack/src/main/java/com/studying/fintrack/domain/models/ImportJobDTO.java
+++ fintrack/src/main/java/com/studying/fintrack/domain/models/ImportJobDTO.java
@@ -2,7 +2,16 @@ package com.studying.fintrack.domain.models;
 
 public class ImportJobDTO {
 
+  private String status;
   private String statsJson;
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
 
   public String getStatsJson() {
     return statsJson;

c9d0af1ffdabf4d2aec056edd5d01fdd44a31f12
ZulGuy <frostborne1@gmail.com>
2026-01-12 22:19:01 +0200

    Created some components for front-end for fintrack.

--- fintrack-front-end/src/app/app-module.ts
+++ fintrack-front-end/src/app/app-module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Users } from './components/users/users';
+import { UsersComponent } from './components/users.component/users.component';
+import { TransactionsComponent } from './components/transactions.component/transactions.component';
+import { AccountsComponent } from './components/accounts.component/accounts.component';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Users,
+    UsersComponent,
+    TransactionsComponent,
+    AccountsComponent
   ],
   imports: [
     BrowserModule,

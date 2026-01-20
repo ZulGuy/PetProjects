6ca7b6fc2733e681cbb96fa7e8837e26a57d9a9b
ZulGuy <frostborne1@gmail.com>
2026-01-20 22:00:24 +0200

    Working on a guide project to study file uploading.

--- gs-uploading-files-main/initial-kotlin/src/main/kotlin/com/example/uploadingfiles/UploadingFilesApplication.kt
+++ gs-uploading-files-main/initial-kotlin/src/main/kotlin/com/example/uploadingfiles/UploadingFilesApplication.kt
@@ -1,9 +1,11 @@
 package com.example.uploadingfiles
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class UploadingFilesApplication
 
 fun main(args: Array<String>) {

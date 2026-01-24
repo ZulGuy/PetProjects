eb99a18c76f19dcb9bbdef009224a42ae351e0e9
ZulGuy <frostborne1@gmail.com>
2026-01-24 22:27:13 +0200

    Working on file managing Finance Tracker service.

---
+++ fintrack-file-manager/src/main/java/com/studying/fintrackfilemanager/storage/StorageProperties.java
@@ -0,0 +1,15 @@
+package com.studying.fintrackfilemanager.storage;
+
+public class StorageProperties {
+
+  private String location = "fileStorage";
+
+  public String getLocation() {
+    return location;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
+
+}

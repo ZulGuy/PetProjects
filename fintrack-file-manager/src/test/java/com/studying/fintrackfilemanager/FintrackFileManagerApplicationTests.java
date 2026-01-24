5456abd0cf0843d0be2a59920cd62701404ebc06
ZulGuy <frostborne1@gmail.com>
2026-01-24 16:44:17 +0200

    Working on file managing Finance Tracker service.

---
+++ fintrack-file-manager/src/test/java/com/studying/fintrackfilemanager/FintrackFileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studying.fintrackfilemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackFileManagerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

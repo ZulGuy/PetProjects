43703485d5516bf7c1a8abb0b7ab43abfd6c6bcc
ZulGuy <frostborne1@gmail.com>
2026-01-24 16:44:17 +0200

    Working on file managing Finance Tracker service.

---
+++ fintrack-file-manager/src/main/java/com/studying/fintrackfilemanager/FintrackFileManagerApplication.java
@@ -0,0 +1,13 @@
+package com.studying.fintrackfilemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackFileManagerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FintrackFileManagerApplication.class, args);
+  }
+
+}

b5a1a614132bad50bca4cc01c61fd0f8f6eae747
ZulGuy <frostborne1@gmail.com>
2026-01-24 21:14:04 +0200

    Working on file managing Finance Tracker service.

---
+++ fintrack-file-manager/src/main/java/com/studying/fintrackfilemanager/storage/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package com.studying.fintrackfilemanager.storage;
+
+public class StorageFileNotFoundException extends RuntimeException {
+
+  public StorageFileNotFoundException(String message) {
+    super(message);
+  }
+
+  public StorageFileNotFoundException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}

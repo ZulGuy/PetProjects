2f8cadd7d5e56fbc6e00b3d0ae17c9a7b74b987e
ZulGuy <frostborne1@gmail.com>
2026-02-09 00:36:57 +0200

    Studying Java Core. Control flow statements

---
+++ exercise1/src/main/java/loop/ForEachLoop.java
@@ -0,0 +1,32 @@
+package loop;
+
+public class ForEachLoop {
+
+  public static void main(String[] args) {
+//    char[] letters = {'h', 'e', 'l', 'l', 'o'};
+//    for (int i = 0; i < letters.length; i++) {
+//      System.out.println(letters[i]);
+//    }
+
+//    //for-each loop
+//    char[] letters = {'h', 'e', 'l', 'l', 'o'};
+//    for (char item : letters) {
+//      System.out.println(item);
+//    }
+
+//    int[] numbers = {6, 4, 3, 5};
+//    int sum = 0;
+//    for (int num : numbers) {
+//      sum += num;
+//      System.out.println("num: " + num);
+//    }
+//
+//    System.out.println("sum: " + sum);
+
+    String movie = "Titanic";
+    for (char eachLetter : movie.toCharArray()) {
+      System.out.print(eachLetter);
+    }
+  }
+
+}

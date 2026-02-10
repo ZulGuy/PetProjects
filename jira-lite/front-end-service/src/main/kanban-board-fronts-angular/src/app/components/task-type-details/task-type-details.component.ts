6dd000254abcb7f3ba69be81948d40ba5db22d31
ZulGuy <frostborne1@gmail.com>
2026-02-10 22:06:59 +0200

    Add jira lite project 2025 year

---
+++ jira-lite/front-end-service/src/main/kanban-board-fronts-angular/src/app/components/task-type-details/task-type-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-type-details',
+  templateUrl: './task-type-details.component.html',
+  styleUrls: ['./task-type-details.component.scss']
+})
+export class TaskTypeDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

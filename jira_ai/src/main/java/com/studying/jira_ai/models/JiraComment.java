3ad12487754a8a626dc236c07427ffb72f6f2d34
Volodymyr Melnyk <90920233+ZulGuy@users.noreply.github.com>
2026-02-10 21:27:45 +0200

    Add jira ai project

---
+++ jira_ai/src/main/java/com/studying/jira_ai/models/JiraComment.java
@@ -0,0 +1,22 @@
+package com.studying.jira_ai.models;
+
+public class JiraComment {
+  private String id;
+  private String body;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getBody() {
+    return body;
+  }
+
+  public void setBody(String body) {
+    this.body = body;
+  }
+}

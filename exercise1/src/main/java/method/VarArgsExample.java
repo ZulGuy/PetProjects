04fd371531887083610503425d0f454ca8ed57b6
ZulGuy <frostborne1@gmail.com>
2026-02-11 23:11:49 +0200

    Studying Java Core. Methods

---
+++ exercise1/src/main/java/method/VarArgsExample.java
@@ -0,0 +1,45 @@
+package method;
+
+import java.util.Scanner;
+
+public class VarArgsExample {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    while (true) {
+
+      System.out.println("Please enter the numbers you want to sum, leave a space between them");
+      String[] inputNumbers = scanner.nextLine().split(" ");
+      sumNumbers(inputNumbers);
+      System.out.println("Do you want to continue, (Y for Yes, N for No)?");
+      char response = scanner.nextLine().charAt(0);
+      if (response == 'N' || response == 'n') {
+        System.out.println("-----Program has terminated-----");
+        break;
+      }
+
+    }
+    scanner.close();
+
+  }
+
+  public static void sumNumbers (String... numbers) {
+
+    int sum = 0;
+    String enteredNumbers = "";
+
+    for (int i = 0; i < numbers.length; i++) {
+
+      sum += Integer.parseInt(numbers[i]);
+      if (i != numbers.length - 1)
+        enteredNumbers += numbers[i] + " + ";
+      else
+        enteredNumbers += numbers[i];
+
+    }
+    System.out.println(enteredNumbers + " = " + sum);
+
+  }
+
+}

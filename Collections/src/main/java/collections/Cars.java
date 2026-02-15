71be13370c2d0fe86c6fe322b1183703d9cbf03e
ZulGuy <frostborne1@gmail.com>
2026-02-16 00:24:26 +0200

    Studying Java Core. Collections

---
+++ Collections/src/main/java/collections/Cars.java
@@ -0,0 +1,28 @@
+package collections;
+
+public class Cars {
+
+  String carName;
+  int year;
+  int price;
+
+
+  //Constructor
+  public Cars(String carName, int year, int price) {
+    this.carName = carName;
+    this.year = year;
+    this.price = price;
+  }
+
+  public String getCarName() {
+    return carName;
+  }
+
+  public int getYear() {
+    return year;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+}
---
+++ Collections/src/main/java/collections/ListExamples.java
@@ -0,0 +1,22 @@
+package collections;
+
+import java.util.*;
+
+public class ListExamples {
+
+  public static void main(String[] args) {
+
+    List<String> list1 = new ArrayList<>();
+    List<Integer> list2;//Declaration
+    list2 = new ArrayList<>();//Initialization
+//    List<double> list3 = new ArrayList<>();//we cannot use primitive type in Collections
+    List<Double> list3 = new ArrayList<>();
+    Collection<Integer> list4 = new ArrayList<>();
+
+    List<String> list5 = new ArrayList<>();
+    list5.add("Hello Java Developers");
+    String message = list5.get(0);
+
+  }
+
+}

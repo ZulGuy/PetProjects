ae3855ec4ffa85b4bd83afbc897e56b2fba132d9
ZulGuy <frostborne1@gmail.com>
2026-02-19 17:05:34 +0200

    Studying Java Core. Exceptions

---
+++ ExceptionHandling/src/main/java/exception/ThrowsException.java
@@ -0,0 +1,24 @@
+package exception;
+
+public class ThrowsException {
+
+  public static void divide() throws NumberFormatException, ArithmeticException {
+    int a = Integer.parseInt("8");
+    int b = Integer.parseInt("0");
+    int c = a / b;
+    System.out.println("c = " + c);
+  }
+
+  public static void main(String[] args) {
+
+    try {
+      divide();
+    } catch (NumberFormatException e) {
+      System.out.println("Invalid number format!");
+    } catch (ArithmeticException e) {
+      System.out.println("You cannot divide number by zero!");
+    }
+
+  }
+
+}

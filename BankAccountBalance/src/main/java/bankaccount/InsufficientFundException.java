fbd97ca50fc2d9e585e128af7076e0147ca2a5c5
ZulGuy <frostborne1@gmail.com>
2026-02-19 23:25:26 +0200

    Studying Java Core. Exceptions

---
+++ BankAccountBalance/src/main/java/bankaccount/InsufficientFundException.java
@@ -0,0 +1,16 @@
+package bankaccount;
+
+public class InsufficientFundException extends Exception {
+
+  private String message;
+
+  //Constructor
+  public InsufficientFundException(String message) {
+    this.message = message;
+  }
+
+  //getter method of message
+  public String getMessage() {
+    return message;
+  }
+}

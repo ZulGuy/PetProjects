3c446ef66551239d372b39b46749229e50ac5872
ZulGuy <frostborne1@gmail.com>
2026-02-20 15:51:56 +0200

    Studying Java Core. Method Refference

--- LambdaExpression/src/main/java/lambdaexpression/MainTest.java
+++ LambdaExpression/src/main/java/lambdaexpression/MainTest.java
@@ -8,13 +8,18 @@ public class MainTest {
 //    Animals animal = new Animals() {
 //      @Override
 //      public void show(String animal, int speed) {
-//        System.out.println(animal + " runs at " + speed + " km/h");
+//        System.out.println("A " + animal + " can reach speed of " + speed + " kilometers per hour");
 //      }
 //    };
+//
+//    animal.show("Cheetah", 90);
 
     //Lambda Expression
-    Animals animal = (a,s) -> System.out.println(a + " runs at " + s + " km/h");
-
+//    Animals animal = (a, s) -> {
+//      System.out.println("A " + a + " can reach speed of " + s + " kilometers per hour");
+//    };
+    Animals animal = (a,s) -> System.out.println("A " + a + " can reach speed of " + s + " kilometers per hour");
+    animal.show("Cheetah", 90);
   }
 
 }
---
+++ LambdaExpression/src/main/java/lambdaexpression/MethodReferenceExample.java
@@ -0,0 +1,27 @@
+package lambdaexpression;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class MethodReferenceExample {
+
+  public static void main(String[] args) {
+
+    Integer.parseInt("123");
+
+//    List<String> animals = List.of("Cat", "Cow", "Dog", "Horse");
+//    Consumer<String> animalConsumer = System.out::println;
+//    animals.forEach(animalConsumer);
+
+//    List<String> animals = List.of("Cat", "Cow", "Dog", "Horse");
+//    Consumer<String> animalConsumer = System.out::println;
+//    animals.forEach(animal -> {
+//      if(animal.startsWith("C")) {
+//        System.out.println(animal);
+//      }
+//    });
+
+  }
+
+}

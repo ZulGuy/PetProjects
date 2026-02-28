8a9a582c4b31bb36f124ce552224192e1de7f5b8
ZulGuy <frostborne1@gmail.com>
2026-02-28 10:56:56 +0200

    Studying Java Core. Date&Time

---
+++ DateTimeOperations/src/main/java/datetimeexamples/LocalTimeExamples.java
@@ -0,0 +1,14 @@
+package datetimeexamples;
+
+import java.time.LocalTime;
+
+public class LocalTimeExamples {
+
+  public static void main(String[] args) {
+
+    LocalTime time = LocalTime.now();
+    System.out.println("Current time: " + time);
+
+  }
+
+}

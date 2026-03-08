9307f43a436ae483c7be5ab3b52af6dba2b05e63
ZulGuy <frostborne1@gmail.com>
2026-03-08 10:06:04 +0200

    Studying Java Core. Threads

--- Threading/src/main/java/MainTest.java
+++ Threading/src/main/java/MainTest.java
@@ -2,8 +2,13 @@ public class MainTest {
 
   public static void main(String[] args) {
 
-    MyThread myThread = new MyThread();
-    myThread.start();
+//    MyThread myThread = new MyThread();
+//    myThread.start();
+//    System.out.println("Thread is started");
+
+    MyRunnable myRunnable = new MyRunnable();
+    Thread thread = new Thread(myRunnable);
+    thread.start();
     System.out.println("Thread is started");
 
   }
---
+++ Threading/src/main/java/MyRunnable.java
@@ -0,0 +1,7 @@
+public class MyRunnable implements Runnable{
+
+  public void run() {
+    System.out.println("Creating thread with Runnable: Thread is running");
+  }
+
+}

651eb60ca4e2539b44e6d9e3f89158916eb4227f
ZulGuy <frostborne1@gmail.com>
2026-03-17 22:22:18 +0200

    ITSD IT Telegram Bot. Created DTOs.

---
+++ ITSD-IT-Bot/src/main/java/com/studying/itsditbot/CurrentStatus.java
@@ -0,0 +1,5 @@
+package com.studying.itsditbot;
+
+public record CurrentStatus(String status) {
+
+}
---
+++ ITSD-IT-Bot/src/main/java/com/studying/itsditbot/Fields.java
@@ -0,0 +1,5 @@
+package com.studying.itsditbot;
+
+public record Fields(String summary, Reporter reporter, CustomField10001 customField10001) {
+
+}

a6d163a1d784dca8dfb7fc7ee8fafc418afd54ba
ZulGuy <frostborne1@gmail.com>
2026-03-17 21:05:22 +0200

    ITSD IT Telegram Bot. Created project.

---
+++ ITSD-IT-Bot-2/src/main/java/Bot.java
@@ -0,0 +1,11 @@
+import org.telegram.telegrambots.meta.generics.TelegramBot;
+
+public class Bot {
+
+  public static void main(String[] args) {
+
+    TelegramBot bot = new TelegramBot();
+
+  }
+
+}
---
+++ ITSD-IT-Bot/src/main/java/com/studying/itsditbot/ItsdItBotApplication.java
@@ -0,0 +1,13 @@
+package com.studying.itsditbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsdItBotApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ItsdItBotApplication.class, args);
+  }
+
+}

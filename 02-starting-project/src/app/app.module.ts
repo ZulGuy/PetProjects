0a1e3d5210598bbeda0ac6276eaf9e525b6519f6
ZulGuy <frostborne1@gmail.com>
2026-04-21 23:48:33 +0300

    Studying Angular. Started learning module-based approach.

--- 02-starting-project/src/app/app.module.ts
+++ 02-starting-project/src/app/app.module.ts
@@ -6,10 +6,12 @@ import {TasksComponent} from "./tasks/tasks.component";
 import {TaskComponent} from "./tasks/task/task.component";
 import {NewTaskComponent} from "./tasks/new-task/new-task.component";
 import {CardComponent} from "./shared/card/card.component";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
-  imports: [],
+  imports: [BrowserModule, FormsModule],
   declarations: [UserComponent, HeaderComponent, AppComponent, TasksComponent, TaskComponent, NewTaskComponent, CardComponent],
-  bootstrap: []
+  bootstrap: [AppComponent]
 })
 export class AppModule {}

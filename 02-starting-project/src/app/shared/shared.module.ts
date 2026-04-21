c8cfb5626da6d2c9457de4959378aec251f67cc7
ZulGuy <frostborne1@gmail.com>
2026-04-21 23:48:33 +0300

    Studying Angular. Started learning module-based approach.

---
+++ 02-starting-project/src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from "@angular/core";
+import {CardComponent} from "./card/card.component";
+
+@NgModule({
+  imports: [],
+  declarations: [CardComponent],
+  exports: [],
+})
+export class SharedModule {
+
+}
